2cedb637b761d95fd637942209aefe78f5198192
carlosrivasesp <202110052@urp.edu.pe>
2025-09-01 10:41:43 -0500

    corregido

--- routes/operacionRoute.js
+++ routes/operacionRoute.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const operacionController = require('../controllers/operacionController');
 
-router.post('/venta/', operacionController.registrarVenta);
-router.post('/compra/', operacionController.registrarCompra);
-router.post('/cotizacion/', operacionController.registrarCotizacion);
+router.post('/venta', operacionController.registrarVenta);
+router.post('/compra', operacionController.registrarCompra);
+router.post('/cotizacion', operacionController.registrarCotizacion);
 router.get('/', operacionController.obtenerOperaciones);
 router.get('/:id', operacionController.obtenerOperacion);
 router.put('/:id/estado', operacionController.actualizarEstado);
